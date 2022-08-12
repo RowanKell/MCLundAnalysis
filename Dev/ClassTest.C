1b0752d0be522a7c31b5d71383c705bd57a36f71
Rowan Kelleher <rck32@duke.edu>
2022-08-12 19:13:42 -0400

    beginning to convert Analysis file to a more clean process using a MCParticle class

---
+++ Dev/ClassTest.C
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "TROOT.h"
+#include "TTree.h"
+#include "TFile.h"
+
+using namespace std;
+double Pfunc(double Px, double Py, double Pz)
+{
+    return sqrt(Px*Px + Py*Py + Pz*Pz);
+}
+
+double Efunc(double M, double P)
+{
+    return sqrt(M * M + P * P);
+}
+
+double Ptfunc(double Px, double Py)
+{
+    return sqrt(Px*Px + Py*Py);
+}
+
+TVector2 PtVectfunc(TLorentzVector lv)
+{
+    TVector2 Pt;
+    double Px = lv.Px();
+    double Py = lv.Py();
+    Pt.SetX(Px);
+    Pt.SetY(Py);
+    return Pt;
+}
+
+double cthfunc(double Px, double Py, double Pz)
+{
+    double Pt = sqrt(Px*Px + Py*Py);
+    return Pz / sqrt(Pz * Pz + Pt * Pt);
+}
+
+double Q2func(double E1, double E2, double cth)
+{
+    return 2 * E1 * E2 * (1.0 - cth);
+}
+
+double yfunc(double E1,double E2)
+{
+    return (E1 - E2) / E1;
+}
+double sfunc(double M1,double M2,double E) //proton mass is M1, electron mass is M2, E is electron beam energy
+{
+    return M1 * M1 + M2 * M2 + 2 * M1 * E;
+}
+
+double R0func(TLorentzVector ki, TLorentzVector kf, TVector2 deltak, double Q2)
+{
+    double init;
+    double final;
+    double delta;
+    
+    double mag;
+    
+    init = abs((ki * ki) / (Q2));
+    final = abs((kf * kf) / (Q2));
+    delta = abs((deltak * deltak) / (Q2));
+    
+    //Selecting the max of the ratios
+    if(init > final && init > delta){mag = init;}
+    else if(final > init && final > delta){mag = final;}
+    else if(delta > init && delta > final){mag = delta;}
+    return mag;
+}
+
+double R1func(TLorentzVector Ph, TLorentzVector ki, TLorentzVector kf)
+{
+    return (Ph * ki) / (Ph * kf);
+}
+
+double R2func(TLorentzVector k, double Q2)
+{
+    return abs(k * k) / Q2;
+}
+
+double Mxfunc(TLorentzVector q, TLorentzVector target, TLorentzVector hadron1, TLorentzVector hadron2)
+{
+    TLorentzVector lv_Mx;
+    lv_Mx = q + target - hadron1 - hadron2;
+    return lv_Mx.M();
+}
+double xFfunc(TLorentzVector p, TLorentzVector q, double W)
+{
+    return 2 * (p.Vect().Dot(q.Vect())) / (q.Vect().Mag() * W);
+}
+
+double nufunc(double E1, double E2)
+{
+  return E1-E2;
+}
+
+double Wfunc(double Q2, double mT, double nu)
+{
+    return sqrt(-Q2 + pow(mT,2) + 2 * mT * nu);
+}
+
+double thetafunc(double pt, double pz)
+{
+    return abs(atan(pt/pz));
+}
+
+double LightConeMinus(TLorentzVector lv)
+{
+    return (lv.E() - lv.Pz()) / (sqrt(2));
+}
+
+double LightConePlus(TLorentzVector lv)
+{
+    return (lv.E() + lv.Pz()) / (sqrt(2));
+}
+//
+//Class stuff
+//
+class MCParticle
+{
+    public:
+    
+    //Lund bank variables
+    int pid = 0;
+    double id = 0;
+    double px = 0;
+    double py = 0;
+    double pz = 0;
+    double daughter = 0;
+    double parent = 0;
+    double mass = 0;
+    double P = 0;
+    double E = 0;
+    //TLorentzVector
+    TLorentzVector lv;
+    
+    //Calculations
+    double Pt = 0;
+    TVector2 PtVect;
+    
+    void inputPxPyPzM(double _px, double _py, double _pz, double _m);
+    
+    void SetParentDaughter(double _parent,double _daughter);
+    
+    
+};
+
+void MCParticle::inputPxPyPzM(double _px, double _py, double _pz, double _m)
+{
+    px = _px;
+    py = _py;
+    pz = _pz;
+    mass = _m;
+    
+    P = Pfunc(px, py, pz);
+    E = Efunc(mass, P);
+    
+    lv.SetPxPyPzE(px,py,pz,E);
+    
+    Pt = Ptfunc(px, py);
+    PtVect = PtVectfunc(lv);
+    
+}
+void MCParticle::SetParentDaughter(double _parent,double _daughter)
+{
+    parent = _parent;
+    daughter = _daughter;
+}
+
+int ClassTest()
+{
+    MCParticle pion;
+    pion.inputPxPyPzM(5,2,1,3);
+    pion.lv.Print();
+    pion.SetParentDaughter(2, 3);
+    pion.pid = 2;
+    cout << "P: " << pion.P << "   PtVect: " << pion.PtVect.Px() << ", " << pion.PtVect.Py() << "\n";
+    cout << "px: " << pion.px << "\n";
+    cout << "E: " << pion.E << '\n';
+    cout << "Parent, Daughter: " << pion.parent << ", " << pion.daughter << "\n";
+    cout << "pid: " << pion.pid << '\n';
+    
+    return 0;
+}
